24283498fd1672ce0fd4dd780bc628999eb13dd2
pradip vasan <vasanpradip06@gmail.com>
2025-05-17 12:28:39 +0530

    Update userServices.js

--- src/services/userServices.js
+++ src/services/userServices.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:5000/api"; 
+const API_URL = "https://de-6not.vercel.app/api"; 
 
 export const gethotels = async (id) => {
     try {
