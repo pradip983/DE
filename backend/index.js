ac41bd9a033e5d86f31143e0ba12096a5aedb010
pradip vasan <vasanpradip06@gmail.com>
2025-05-17 17:02:09 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -18,7 +18,7 @@ const app = express();
 
 // CORS config
 app.use(cors({
-  origin: 'http://localhost:5173',
+  origin: 'https://de-red.vercel.app',
   methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true
